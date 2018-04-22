8e2f8c7269b0a8ad8aa45db7d66c9372ca011c6f
Şafak Çıplak <safakciplak1990@gmail.com>
2018-04-22 16:13:50 +0300

    touches1 react-nativebutton

---
+++ components/Touch.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {View, Alert, StyleSheet} from 'react-native';
+import Button from 'react-native-button';
+
+export default class Touch extends Component {
+  _onPressButton = () => {
+    Alert.alert("You pressed the button!")
+  }
+  render(){
+    return(
+      <View style={{
+          flex:1,
+          justifyContent:'center',
+          alignItems:'center'
+      }}>
+
+      <Button style={{
+        fontSize:20,
+        color:'white',
+        backgroundColor:'green',
+        padding:10,
+        borderRadius:20
+      }}
+      onPress={this._onPressButton}
+      >This is a button</Button>
+      
+      </View>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import FixedDimension from './components/FixedDimension';
 import FlexDimension from './components/FlexDimension';
 import FlexExample from './components/FlexExample';
 import JustifyContentExample from './components/JustifyContentExample';
-import TextInput from './components/TextInput';
+import Touch from './components/Touch';
 import App from './App';
 
-AppRegistry.registerComponent('reactnative', () => TextInput);
+AppRegistry.registerComponent('reactnative', () => Touch);
