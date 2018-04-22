3dc2031f4c54563d91417cddc3d0850138867d7f
Şafak Çıplak <safakciplak1990@gmail.com>
2018-04-22 14:37:24 +0300

    text input working with multiple line TextInput and Keyboard

--- components/TextInput.js
+++ components/TextInput.js
@@ -1,15 +1,36 @@
 import React, {Component} from 'react';
-import {TextInput, View, Text} from 'react-native';
+import {TextInput, View, Text, Keyboard} from 'react-native';
 
 export default class Robot extends Component {
   constructor(props){
     super(props);
     this.state={
       typedText:'please type your text',
-      typedPassword:'please type your password'
+      typedPassword:'',
+      typedDescription:'',
     }
   }
+
+  componentWillMount(){
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+      this.setState(() => {
+        return {typedText: 'Keyboard is shown'}
+      });
+    });
+
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidHide', () => {
+      this.setState(() => {
+        return {typedText: 'Keyboard Hide'}
+      });
+    });
+  }
+  componentWillUnMount(){
+    this.keyboardDidShowListener.remove();
+  }
+
   render(){
+
+
 
     return(
       <View>
@@ -46,12 +67,38 @@ export default class Robot extends Component {
           keyboardType='email-address'
           placeholder='Enter your password'
           secureTextEntry={true}
+          //autoFocus={true}
           onChangeText={(text) => {this.setState(() => {
             return {
               typedPassword: text
             };
           })}}
         />
+
+        <TextInput style={{
+          height:100,
+          margin:20,
+          padding:10,
+          borderColor:'grey',
+          borderWidth:1,
+        }}
+        multine={true}
+        borderBottomColor='green'
+        borderBottomWidth={3}
+        borderRightColor='green'
+        borderRightWidth={3}
+        borderLeftColor='green'
+        borderLeftWidth={3}
+        editable={true}
+        onSubmitEditing={Keyboard.dismiss}
+        returnKeyType='done'
+        onChangeText={(text) => {this.setState(() => {
+          return {
+            typedDescription: text
+          };
+        })}}
+        />
+
       </View>
     );
   }
